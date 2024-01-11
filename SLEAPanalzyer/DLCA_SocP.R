d49119d64e0e7fe2a65526ca2c33fc1dca82cf6f
topohl <119701302+topohl@users.noreply.github.com>
2024-01-11 18:11:12 +0100

    Refactor code: Install and load required packages, modified replace NAs, cleanup

--- SLEAPanalzyer/DLCA_SocP.R
+++ SLEAPanalzyer/DLCA_SocP.R
@@ -1,100 +1,92 @@
-# install tensor flow and prepare for libraries and add new environment r-reticulate
-install.packages("tensorflow")
-library(reticulate)
-path_to_python <- install_python()
-virtualenv_create("r-reticulate", python = path_to_python)
-library(tensorflow)
-install_tensorflow(envname = "r-reticulate")
-install.packages("keras")
-library(keras)
-install_keras(envname = "r-reticulate")
-library(tensorflow)
+# Check if packages are installed, install them if needed, and load them
+requiredPackages <- c("tensorflow", "reticulate", "keras", "sp", "imputeTS", "ggplot2", "ggmap", "data.table", "cowplot", "corrplot", "zoo")
 
-# Load required libraries
-library(sp)
-library(imputeTS)
-library(ggplot2)
-library(ggmap)
-library(data.table)
-library(cowplot)
-library(corrplot)
-library(keras)
-library(tensorflow)
-library(zoo)
+for (package in requiredPackages) {
+  if (!requireNamespace(package, quietly = TRUE)) {
+    install.packages(package)
+  }
+  library(package, character.only = TRUE)
+}
 
 # Set working directory and load R script
 setwd("C:/Users/topohl/Documents/GitHub/DLCAnalyzer")
 source('R/DLCAnalyzer_Functions_final.R')
 
 # Set input and output directories
-input_dir <- "C:/Users/topohl/Documents/GitHub/sleap/docs/notebooks"
-output_dir <- "C:/Users/topohl/Documents/GitHub/sleap/docs/notebooks/output"
+inputDir <- "C:/Users/topohl/Documents/GitHub/sleap/docs/notebooks"
+outputDir <- "C:/Users/topohl/Documents/GitHub/sleap/docs/notebooks/output"
 
 # Get a list of CSV files in the input directory
-file_list <- list.files(path = input_dir, pattern = "*.csv")
+fileList <- list.files(path = inputDir, pattern = "*.csv")
 
-# create new list df_list
-df_list <- list()
+# create new list dfList
+dfList <- list()
+
+# Function to replace NAs in specified columns of a data frame with the last known values
+replaceNAs <- function(tracking, columns) {
+  for (col in columns) {
+      tracking$data[[col]] <- zoo::na.locf(tracking$data[[col]])
+  }
+  return(tracking)
+}
+
+# Define the names of the columns to replace NAs
+replaceNABodyparts <- c("nose", "bodycentre")
 
 # Loop through each file in the input directory
-for (file in file_list) {
+for (file in fileList) {
   
   # Read in tracking data and get names of variables in the data frame
-  input_file <- file.path(input_dir, file)
-  Tracking <- ReadDLCDataFromCSV(file = input_file, fps = 30)
+  inputFile <- file.path(inputDir, file)
+  tracking <- ReadDLCDataFromCSV(file = inputFile, fps = 30)
 
   # Extract the input file name without the extension
-  input_file_name <- sub(".csv$", "", basename(input_file))
+  inputFileName <- sub(".csv$", "", basename(inputFile))
 
-  # Replace NAs in the x and y columns of the nose data frame with the last known values
-  Tracking$data$nose$x <- zoo::na.locf(Tracking$data$nose$x)
-  Tracking$data$nose$y <- zoo::na.locf(Tracking$data$nose$y)
-  
+  # Call the replaceNAs function with the defined column names
+  tracking <- replaceNAs(tracking, replaceNABodyparts)
+
   # Calibrate the tracking data, add zones, and plot the zones
-  Tracking <- CalibrateTrackingData(Tracking, method = "area",in.metric = 44*24, points = c("tl","tr","br","bl"))
-  Tracking <- AddOFTZones(Tracking, scale_center = 0.5,scale_periphery  = 0.8 ,scale_corners = 0.4, points = c("tl","tr","br","bl"))
-  PlotZones(Tracking)
-  PlotPointData(Tracking, points = c("nose"))
+  tracking <- CalibrateTrackingData(tracking, method = "area",in.metric = 44*24, points = c("tl","tr","br","bl"))
+  tracking <- AddOFTZones(tracking, scale_center = 0.5,scale_periphery  = 0.8 ,scale_corners = 0.4, points = c("tl","tr","br","bl"))
+  PlotZones(tracking)
+  PlotPointData(tracking, points = c("nose"))
 
   # Calculate time_Left and time_Right direct
-  total_time <- 10 * 60
-  IsInZoneLeft <- GetDistances(Tracking, "socl", "nose") < 6
-  IsInZoneRight <- GetDistances(Tracking, "socr", "nose") < 6
-  ContactLeft <- sum(IsInZoneLeft) * total_time / length(IsInZoneLeft)
-  ContactRight <- sum(IsInZoneRight) * total_time / length(IsInZoneRight)
+  totalTime <- 10 * 60
+  isInZoneLeft <- GetDistances(tracking, "socl", "nose") < 6
+  isInZoneRight <- GetDistances(tracking, "socr", "nose") < 6
+  contactLeft <- sum(isInZoneLeft) * totalTime / length(isInZoneLeft)
+  contactRight <- sum(isInZoneRight) * totalTime / length(isInZoneRight)
   
   # Calculate time with nose between 6-10 cm of either socl or socr
-  IsInProxLeft <- GetDistances(Tracking, "socl", "nose") >= 6 & GetDistances(Tracking, "socl", "nose") <= 10
-  IsInProxRight <- GetDistances(Tracking, "socr", "nose") >= 6 & GetDistances(Tracking, "socr", "nose") <= 10
-  ProxLeft <- sum(IsInProxLeft) * total_time / length(IsInProxLeft)
-  ProxRight <- sum(IsInProxRight) * total_time / length(IsInProxRight)
+  isInProxLeft <- GetDistances(tracking, "socl", "nose") >= 6 & GetDistances(tracking, "socl", "nose") <= 10
+  isInProxRight <- GetDistances(tracking, "socr", "nose") >= 6 & GetDistances(tracking, "socr", "nose") <= 10
+  proxLeft <- sum(isInProxLeft) * totalTime / length(isInProxLeft)
+  proxRight <- sum(isInProxRight) * totalTime / length(isInProxRight)
 
   # Create a data frame with the time_Left, time_Right, time_nose_Left, and time_nose_Right values
-  df <- data.frame(file = input_file_name, ContactLeft = ContactLeft, ContactRight = ContactRight, 
-                 ProxLeft = ProxLeft, ProxRight = ProxRight)
+  df <- data.frame(file = inputFileName, contactLeft = contactLeft, contactRight = contactRight, 
+                 proxLeft = proxLeft, proxRight = proxRight)
 
   # Construct the output file name
-  output_file <- paste0(input_file_name, "_output.csv")
+  outputFile <- paste0(inputFileName, "_output.csv")
 
   # Write the data frame to a csv file with the constructed file name
-  write.csv(df, output_file, row.names = FALSE)
+  write.csv(df, outputFile, row.names = FALSE)
 
   # Add the current data frame to the list
-  df_list[[length(df_list) + 1]] <- df
+  dfList[[length(dfList) + 1]] <- df
 
   # Print progress message
   cat(sprintf("Processed file %s\n", file))
 }
 
 # Combine all data frames into a single data frame
-df_combined <- do.call(rbind, df_list)
+dfCombined <- do.call(rbind, dfList)
 
 # Write the combined data frame to a csv file
-write.csv(df_combined, file.path(output_dir, "combined_output.csv"), row.names = FALSE)
+write.csv(dfCombined, file.path(outputDir, "combined_output.csv"), row.names = FALSE)
 
 # Print "done" message
 cat("done\n")
-
-
-
-
