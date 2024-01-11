b2212b66e75432f7d4d10d2bba26551183ab31e4
topohl <119701302+topohl@users.noreply.github.com>
2024-01-11 12:00:23 +0100

    Update DLCA_NOR v.1.0.2a.R
    + create function to calculate orientation angle "calcAngle"
    + optimization of angle calculation
    + code cleanup and improvements to readability

--- SLEAPanalzyer/testing/DLCA_NOR v.1.0.2a.R
+++ SLEAPanalzyer/testing/DLCA_NOR v.1.0.2a.R
@@ -1,4 +1,3 @@
-
 # Load required libraries
 library(sp)
 library(imputeTS)
@@ -16,27 +15,42 @@ setwd("C:/Users/topohl/Documents/GitHub/DLCAnalyzer")
 source('R/DLCAnalyzer_Functions_final.R')
 
 # Set input and output directories
-input_dir <- "S:/Lab_Member/Tobi/Experiments/Exp9_Social-Stress/Raw Data/Behavior/B3/NOR/SLEAP/formatted"
-output_dir <- "S:/Lab_Member/Tobi/Experiments/Exp9_Social-Stress/Raw Data/Behavior/B3/NOR/SLEAP/testoutput"
+inputDir <- "S:/Lab_Member/Tobi/Experiments/Exp9_Social-Stress/Raw Data/Behavior/B3/NOR/SLEAP/formatted"
+outputDir <- "S:/Lab_Member/Tobi/Experiments/Exp9_Social-Stress/Raw Data/Behavior/B3/NOR/SLEAP/testoutput"
 # Create a new folder for saving the plots
-plot_dir <- file.path(output_dir, "plots")
-dir.create(plot_dir, showWarnings = FALSE)
+plotDir <- file.path(outputDir, "plots")
+dir.create(plotDir, showWarnings = FALSE)
 
 # Get a list of CSV files in the input directory
-file_list <- list.files(path = input_dir, pattern = "*.csv")
+fileList <- list.files(path = inputDir, pattern = "*.csv")
 
-# create a new list df_list
-df_list <- list()
+# create a new list dfList
+dfList <- list()
+
+calcAngle <- function(vecNose, vecBodyCenter) {
+    # Calculate dot product
+    dotProd <- sum(vecNose * vecBodyCenter)
+
+    # Calculate magnitudes
+    magA <- sqrt(sum(vecNose^2))
+    magC <- sqrt(sum(vecBodyCenter^2))
+
+    # Calculate the angle in radians
+    angleRad <- acos(dotProd / (magA * magC))
+
+    # Convert angle to degrees and return
+    return(angleRad * 180 / pi)
+}
 
 # Loop through each file in the input directory
-for (file in file_list) {
+for (file in fileList) {
   
   # Read in tracking data and get names of variables in the data frame
-  input_file <- file.path(input_dir, file)
-  Tracking <- ReadDLCDataFromCSV(file = input_file, fps = 30)
+  inputFile <- file.path(inputDir, file)
+  Tracking <- ReadDLCDataFromCSV(file = inputFile, fps = 30)
   
   # Extract the input file name without the extension
-  input_file_name <- sub(".csv$", "", basename(input_file))
+  inputFileName <- sub(".csv$", "", basename(inputFile))
   
   # Replace NAs in the x and y columns of the nose data frame with the last known values
   Tracking$data$nose$x <- zoo::na.locf(Tracking$data$nose$x)
@@ -53,133 +67,103 @@ for (file in file_list) {
   Tracking <- OFTAnalysis(Tracking, points = "bodycentre", movement_cutoff = 5, integration_period = 5)
   
   # Create a new data frame to store vector, distance, angle, and circle values
-  df_tracking <- data.frame(
-    vectorNoseBodycentre = cbind((Tracking$data$nose$x - Tracking$data$bodycentre$x), (Tracking$data$nose$y - Tracking$data$bodycentre$y)),
-    vectorNoseObjL = cbind((Tracking$data$nose$x - Tracking$data$objL$x), (Tracking$data$nose$y - Tracking$data$objL$y)),
-    vectorNoseObjR = cbind((Tracking$data$nose$x - Tracking$data$objR$x), (Tracking$data$nose$y - Tracking$data$objR$y)),
-    angleL = acos(sum((Tracking$data$nose$x - Tracking$data$bodycentre$x) * (Tracking$data$objL$x - Tracking$data$bodycentre$x) + (Tracking$data$nose$y - Tracking$data$bodycentre$y) * (Tracking$data$objL$y - Tracking$data$bodycentre$y)) / (sqrt(sum((Tracking$data$nose$x - Tracking$data$bodycentre$x)^2 + (Tracking$data$nose$y - Tracking$data$bodycentre$y)^2)) * sqrt(sum((Tracking$data$objL$x - Tracking$data$bodycentre$x)^2 + (Tracking$data$objL$y - Tracking$data$bodycentre$y)^2)))),
-    angleR = acos(sum((Tracking$data$nose$x - Tracking$data$bodycentre$x) * (Tracking$data$objR$x - Tracking$data$bodycentre$x) + (Tracking$data$nose$y - Tracking$data$bodycentre$y) * (Tracking$data$objR$y - Tracking$data$bodycentre$y)) / (sqrt(sum((Tracking$data$nose$x - Tracking$data$bodycentre$x)^2 + (Tracking$data$nose$y - Tracking$data$bodycentre$y)^2)) * sqrt(sum((Tracking$data$objR$x - Tracking$data$bodycentre$x)^2 + (Tracking$data$objR$y - Tracking$data$bodycentre$y)^2)))),
+  dfTracking <- data.frame(
+    vecNoseBodycentre = cbind((Tracking$data$nose$x - Tracking$data$bodycentre$x), (Tracking$data$nose$y - Tracking$data$bodycentre$y)),
+    vecNoseObjL = cbind((Tracking$data$nose$x - Tracking$data$objL$x), (Tracking$data$nose$y - Tracking$data$objL$y)),
+    vecNoseObjR = cbind((Tracking$data$nose$x - Tracking$data$objR$x), (Tracking$data$nose$y - Tracking$data$objR$y)),
     circleL = sqrt((Tracking$data$objL$x - Tracking$data$bodycentre$x)^2 + (Tracking$data$objL$y - Tracking$data$bodycentre$y)^2),
     circleR = sqrt((Tracking$data$objR$x - Tracking$data$bodycentre$x)^2 + (Tracking$data$objR$y - Tracking$data$bodycentre$y)^2)
   )
   
   # Create new variables in the data frame to indicate if the circle is within 4cm
-  df_tracking$circleL <- ifelse(df_tracking$circleL <= 4, 1, 0)
-  df_tracking$circleR <- ifelse(df_tracking$circleR <= 4, 1, 0)
+  dfTracking$circleL <- ifelse(dfTracking$circleL <= 4, 1, 0)
+  dfTracking$circleR <- ifelse(dfTracking$circleR <= 4, 1, 0)
   
   # Initialize vectors to store angles
-  anglesDegreesObjL <- numeric(length = nrow(df_tracking))
-  anglesDegreesObjR <- numeric(length = nrow(df_tracking))
+  angleDegObjL <- numeric(length = nrow(dfTracking))
+  angleDegObjR <- numeric(length = nrow(dfTracking))
   
-  # Loop through each row for vectorNoseObjL
-for (i in 1:nrow(df_tracking)) {
-  # Extract vectors a and c from vectorNoseObjL and vector matrices
-  a <- c(df_tracking$vectorNoseObjL.1[i], df_tracking$vectorNoseObjL.2[i])
-  c <- c(df_tracking$vectorNoseBodycentre.1[i], df_tracking$vectorNoseBodycentre.2[i])
+# Loop through each row of dfTracking
+for (i in 1:nrow(dfTracking)) {
+    # Extract vectors a and c from vectorNoseObjL and vector matrices
+    vecNoseObjL <- c(dfTracking$vecNoseObjL.1[i], dfTracking$vecNoseObjL.2[i])
+    vecNoseObjR <- c(dfTracking$vecNoseObjR.1[i], dfTracking$vecNoseObjR.2[i])
+    vecBodyCentre <- c(dfTracking$vecNoseBodycentre.1[i], dfTracking$vecNoseBodycentre.2[i])
 
-  # Calculate dot product
-  dotProductAC <- sum(a * c)
-
-  # Calculate magnitudes
-  magnitudeA <- sqrt(sum(a^2))
-  magnitudeC <- sqrt(sum(c^2))
-
-  # Calculate the angle in radians
-  angleRadiansAC <- acos(dotProductAC / (magnitudeA * magnitudeC))
-
-  # Convert angle to degrees and store in the vector
-  anglesDegreesObjL[i] <- angleRadiansAC * 180 / pi
-}
-  
-  # Loop through each row for vectorNoseObjR
-for (i in 1:nrow(df_tracking)) {
-    # Extract vectors a and c from vectorNoseObjR and vector matrices
-    a <- c(df_tracking$vectorNoseObjR.1[i], df_tracking$vectorNoseObjR.2[i])
-    c <- c(df_tracking$vectorNoseBodycentre.1[i], df_tracking$vectorNoseBodycentre.2[i])
-
-    # Calculate dot product
-    dotProductAC <- sum(a * c)
-
-    # Calculate magnitudes
-    magnitudeA <- sqrt(sum(a^2))
-    magnitudeC <- sqrt(sum(c^2))
-
-    # Calculate the angle in radians
-    angleRadiansAC <- acos(dotProductAC / (magnitudeA * magnitudeC))
-
-    # Convert angle to degrees and store in the vector
-    anglesDegreesObjR[i] <- angleRadiansAC * 180 / pi
+    # Calculate angles and store in the vectors
+    angleDegObjL[i] <- calcAngle(vecNoseObjL, vecBodyCentre)
+    angleDegObjR[i] <- calcAngle(vecNoseObjR, vecBodyCentre)
 }
    
   # Calculate time_Left and time_Right only if angle L or R is between 90 and 270 degrees
-  total_time <- 10 * 60
-  IsInZoneLeft <- GetDistances(Tracking, "objL", "nose") <= 4 & GetDistances(Tracking, "objL", "bodycentre") > 1 & abs(anglesDegreesObjL) >= 90 & abs(anglesDegreesObjL) <= 270
-  IsInZoneRight <- GetDistances(Tracking, "objR", "nose") <= 4 & GetDistances(Tracking, "objR", "bodycentre") > 1 & abs(anglesDegreesObjR) >= 90 & abs(anglesDegreesObjR) <= 270
-  ContactLeft <- sum(IsInZoneLeft) * total_time / length(IsInZoneLeft)
-  ContactRight <- sum(IsInZoneRight) * total_time / length(IsInZoneRight)
+  totalTime <- 10 * 60
+  isInZoneLeft <- GetDistances(Tracking, "objL", "nose") <= 4 & GetDistances(Tracking, "objL", "bodycentre") > 1 & abs(angleDegObjL) >= 90 & abs(angleDegObjL) <= 270
+  isInZoneRight <- GetDistances(Tracking, "objR", "nose") <= 4 & GetDistances(Tracking, "objR", "bodycentre") > 1 & abs(angleDegObjR) >= 90 & abs(angleDegObjR) <= 270
+  contactLeft <- sum(isInZoneLeft) * totalTime / length(isInZoneLeft)
+  contactRight <- sum(isInZoneRight) * totalTime / length(isInZoneRight)
   
   # Calculate time with nose between 6-10 cm of either objL or objR
-  IsInProxLeft <- GetDistances(Tracking, "objL", "nose") > 4 & GetDistances(Tracking, "objL", "nose") <= 8
-  IsInProxRight <- GetDistances(Tracking, "objR", "nose") > 4 & GetDistances(Tracking, "objR", "nose") <= 8
-  ProxLeft <- sum(IsInProxLeft) * total_time / length(IsInProxLeft)
-  ProxRight <- sum(IsInProxRight) * total_time / length(IsInProxRight)
+  isInProxLeft <- GetDistances(Tracking, "objL", "nose") > 4 & GetDistances(Tracking, "objL", "nose") <= 8
+  isInProxRight <- GetDistances(Tracking, "objR", "nose") > 4 & GetDistances(Tracking, "objR", "nose") <= 8
+  proxLeft <- sum(isInProxLeft) * totalTime / length(isInProxLeft)
+  proxRight <- sum(isInProxRight) * totalTime / length(isInProxRight)
   
   # Calculate frequency of zone visits for objL and nose
-  IsInZoneVisitL <- cumsum(GetDistances(Tracking, "objL", "nose") <= 4) > 0 & c(0, diff(GetDistances(Tracking, "objL", "nose") <= 4)) == 1
-  FrequencyL <- sum(IsInZoneVisitL)
-  IsInZoneVisitR <- cumsum(GetDistances(Tracking, "objR", "nose") <= 4) > 0 & c(0, diff(GetDistances(Tracking, "objR", "nose") <= 4)) == 1
-  FrequencyR <- sum(IsInZoneVisitR)
+  isInZoneVisitL <- cumsum(GetDistances(Tracking, "objL", "nose") <= 4) > 0 & c(0, diff(GetDistances(Tracking, "objL", "nose") <= 4)) == 1
+  isInZoneVisitR <- cumsum(GetDistances(Tracking, "objR", "nose") <= 4) > 0 & c(0, diff(GetDistances(Tracking, "objR", "nose") <= 4)) == 1
+  frequencyL <- sum(isInZoneVisitL)
+  frequencyR <- sum(isInZoneVisitR)
   
   # Calculate frequency of rearings
-  FreqRear <- cumsum(GetDistances(Tracking, "spine1", "bodycentre") <= 1) > 0 &
+  freqRear <- cumsum(GetDistances(Tracking, "spine1", "bodycentre") <= 1) > 0 &
     cumsum(GetDistances(Tracking, "bodycentre", "spine2") <= 1) > 0 &
     c(0, diff(GetDistances(Tracking, "spine1", "bodycentre") <= 1)) == 1 &
     c(0, diff(GetDistances(Tracking, "bodycentre", "spine2") <= 1)) == 1
-  FrequencyRear <- sum(FreqRear)
+  frequencyRear <- sum(freqRear)
   
   # Create a data frame with the time_Left, time_Right, time_nose_Left, and time_nose_Right values
   df <- data.frame(
-    file = input_file_name,
-    ContactLeft = ContactLeft,
-    ContactRight = ContactRight,
-    ProxLeft = ProxLeft,
-    ProxRight = ProxRight,
-    FrequencyL = FrequencyL,
-    FrequencyR = FrequencyR,
+    file = inputFileName,
+    contactLeft = contactLeft,
+    contactRight = contactRight,
+    proxLeft = proxLeft,
+    proxRight = proxRight,
+    frequencyL = frequencyL,
+    frequencyR = frequencyR,
     distance = Tracking$Report$bodycentre.raw.distance,
     stationary = Tracking$Report$bodycentre.time.stationary,
-    speed_moving = Tracking$Report$bodycentre.speed.moving,
-    speed_raw = Tracking$Report$bodycentre.raw.speed,
-    FrquencyRear = FrequencyRear
+    speedMoving = Tracking$Report$bodycentre.speed.moving,
+    speedRaw = Tracking$Report$bodycentre.raw.speed,
+    frequencyRear = frequencyRear
   )
   
   # Construct the output file name
-  output_file <- paste0(input_file_name, "_output.csv")
+  outputFile <- paste0(inputFileName, "_output.csv")
   
   # Write the data frame to a csv file with the constructed file name
-  write.csv(df, output_file, row.names = FALSE)
+  write.csv(df, outputFile, row.names = FALSE)
   
   # Add the current data frame to the list
-  df_list[[length(df_list) + 1]] <- df
+  dfList[[length(dfList) + 1]] <- df
   
   # Plot the density paths for bodycentre
   plots <- PlotDensityPaths(Tracking, points = c("bodycentre"))
   
   # Set the file name for the plot image
-  plot_file <- file.path(plot_dir, paste0(input_file_name, "_DensityPath.png"))
+  plotFile <- file.path(plotDir, paste0(inputFileName, "_DensityPath.png"))
   
     # Save the plot image with the file name
-    ggsave(plot_file, plot = plots$bodycentre, width = 7, height = 6)
+    ggsave(plotFile, plot = plots$bodycentre, width = 7, height = 6)
     
     # Print progress message
     cat(sprintf("Processed file %s\n", file))
 }
 
 # Combine all data frames into a single data frame
-df_combined <- do.call(rbind, df_list)
+dfCombined <- do.call(rbind, dfList)
 
 # Write the combined data frame to a csv file
-write.csv(df_combined, file.path(output_dir, "combined_output.csv"), row.names = FALSE)
+write.csv(dfCombined, file.path(outputDir, "combined_output.csv"), row.names = FALSE)
 
 # Print "done" message
 cat("done\n")
