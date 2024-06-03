2157d88cc77938e70ab2419750fccee0fe740189
topohl <119701302+topohl@users.noreply.github.com>
2024-06-03 14:59:13 +0200

    update to 1.1.0
    + code cleaning and optimization
    + added batch processing of batches and phases (habituation, S1, and S2)

--- SLEAPanalzyer/testing/DLCA_SocP v.1.0.1.R
+++ SLEAPanalzyer/testing/DLCA_SocP v.1.0.1.R
@@ -1,14 +1,13 @@
-# install tensor flow and prepare for libraries and add new environment r-reticulate
-install.packages("tensorflow")
-library(reticulate)
-path_to_python <- install_python()
-virtualenv_create("r-reticulate", python = path_to_python)
-library(tensorflow)
-install_tensorflow(envname = "r-reticulate")
-install.packages("keras")
-library(keras)
-install_keras(envname = "r-reticulate")
-library(tensorflow)
+# Install required packages
+requiredPackages <- c("data.table", "stringr", "sp", "imputeTS", "ggplot2", "ggmap", "data.table", "cowplot", "corrplot", "keras", "tensorflow", "zoo", "dplyr", "reticulate")
+
+# Check if packages are installed, if not install and load them
+for (package in requiredPackages) {
+  if (!requireNamespace(package, quietly = TRUE)) {
+    install.packages(package)
+  }
+  library(package, character.only = TRUE)
+}
 
 # Load required libraries
 library(sp)
@@ -27,99 +26,161 @@ library(av)
 setwd("C:/Users/topohl/Documents/GitHub/DLCAnalyzer")
 source('R/DLCAnalyzer_Functions_final.R')
 
-# Set input and output directories
-input_dir <- "S:/Lab_Member/Tobi/Experiments/Exp9_Social-Stress/Raw Data/Behavior/B3/SocP/SLEAP/formatted"
-output_dir <- "S:/Lab_Member/Tobi/Experiments/Exp9_Social-Stress/Raw Data/Behavior/B3/SocP/SLEAP/output"
-# Create a new folder for saving the plots
-plot_dir <- file.path(output_dir, "plots")
-dir.create(plot_output_dir, showWarnings = FALSE)
+# Define batch and socpPhase
+batches <- c("B1", "B2", "B3", "B4", "B5", "B6")
+socpPhases <- c("HAB", "S1", "S2")
 
-# Get a list of CSV files in the input directory
-file_list <- list.files(path = input_dir, pattern = "*.csv")
-
-# create new list df_list
-df_list <- list()
-
-# Loop through each file in the input directory
-for (file in file_list) {
-  
-  # Read in tracking data and get names of variables in the data frame
-  input_file <- file.path(input_dir, file)
-  Tracking <- ReadDLCDataFromCSV(file = input_file, fps = 30)
-  
-  # Extract the input file name without the extension
-  input_file_name <- sub(".csv$", "", basename(input_file))
-  
-  # Replace NAs in the x and y columns of the nose data frame with the last known values
-  #Tracking$data$nose$x <- zoo::na.locf(Tracking$data$nose$x)
-  #Tracking$data$nose$y <- zoo::na.locf(Tracking$data$nose$y)
-  #Tracking$data$bodycentre$x <- zoo::na.locf(Tracking$data$bodycentre$x)
-  #Tracking$data$bodycentre$y <- zoo::na.locf(Tracking$data$bodycentre$y)
-  
-  # Calibrate the tracking data, add zones, and plot the zones
-  Tracking <- CalibrateTrackingData(Tracking, method = "area",in.metric = 44*24, points = c("tl","tr","br","bl"))
-  Tracking <- AddOFTZones(Tracking, scale_center = 0.5,scale_periphery  = 0.8 ,scale_corners = 0.4, points = c("tl","tr","br","bl"))
-  PlotZones(Tracking)
-  PlotPointData(Tracking, points = c("nose"))
-  
-  Tracking <- OFTAnalysis(Tracking, points = "bodycentre", movement_cutoff = 5, integration_period = 5)
-  
-  # Calculate time_Left and time_Right direct
-  total_time <- 10 * 60
-  IsInZoneLeft <- GetDistances(Tracking, "socl", "nose") <= 6 & GetDistances(Tracking, "socl", "bodycentre") > 1
-  IsInZoneRight <- GetDistances(Tracking, "socr", "nose") <= 6 & GetDistances(Tracking, "socr", "bodycentre") > 1
-  ContactLeft <- sum(IsInZoneLeft) * total_time / length(IsInZoneLeft)
-  ContactRight <- sum(IsInZoneRight) * total_time / length(IsInZoneRight)
-  
-  # Check if the nose is in the contact zone of objR
-  isContactsocr <- ifelse(IsInZoneRight, "contact socr", "")
-  # Check if the nose is in the contact zone of objL
-  isContactsocl <- ifelse(IsInZoneLeft, "contact socl", "")
-  
-  # Calculate time with nose between 6-10 cm of either objL or objR
-  IsInProxLeft <- GetDistances(Tracking, "socl", "nose") > 6 & GetDistances(Tracking, "socl", "nose") <= 10
-  IsInProxRight <- GetDistances(Tracking, "socr", "nose") > 6 & GetDistances(Tracking, "socr", "nose") <= 10
-  ProxLeft <- sum(IsInProxLeft) * total_time / length(IsInProxLeft)
-  ProxRight <- sum(IsInProxRight) * total_time / length(IsInProxRight)
-  
-  # Calculate frequency of zone visits for objL and nose
-  IsInZoneVisitL <- cumsum(GetDistances(Tracking, "socl", "nose") <= 6) > 0 & c(0, diff(GetDistances(Tracking, "socl", "nose") <= 6)) == 1
-  FrequencyL <- sum(IsInZoneVisitL)
-  IsInZoneVisitR <- cumsum(GetDistances(Tracking, "socr", "nose") <= 6) > 0 & c(0, diff(GetDistances(Tracking, "socr", "nose") <= 6)) == 1
-  FrequencyR <- sum(IsInZoneVisitR)
-  
-  # Create a data frame with the time_Left, time_Right, time_nose_Left, and time_nose_Right values
-  df <- data.frame(file = input_file_name, ContactLeft = ContactLeft, ContactRight = ContactRight,
-                   ProxLeft = ProxLeft, ProxRight = ProxRight, FrequencyL = FrequencyL, FrequencyR = FrequencyR, distance = Tracking$Report$bodycentre.raw.distance, stationary = Tracking$Report$bodycentre.time.stationary, speed_moving = Tracking$Report$bodycentre.speed.moving, speed_raw = Tracking$Report$bodycentre.raw.speed)
-  
-  # Construct the output file name
-  output_file <- paste0(input_file_name, "_output.csv")
-  
-  # Write the data frame to a csv file with the constructed file name
-  write.csv(df, output_file, row.names = FALSE)
-  
-  # Add the current data frame to the list
-  df_list[[length(df_list) + 1]] <- df
-  
-  # Plot the density paths for bodycentre
-  plots <- PlotDensityPaths(Tracking, points = c("bodycentre"))
-  
-  # Set the file name for the plot image
-  plot_file <- file.path(plot_dir, paste0(input_file_name, "_DensityPath.png"))
-  
-  # Save the plot image with the file name
-  ggsave(plot_file, plot = plots$bodycentre, width = 7, height = 4)
-  
-  # Print progress message
-  cat(sprintf("Processed file %s\n", file))
-  
+# Loop through each batch
+for (batch in batches) {
+  # Loop through each socpPhase
+  for (socpPhase in socpPhases) {
+    # Set input and output directories depending on the batch and socpPhase
+    inputDir <- paste0("S:/Lab_Member/Tobi/Experiments/Exp9_Social-Stress/Raw Data/Behavior/", batch, "/SocP/SLEAP/formatted/", socpPhase)
+    outputDir <- paste0("S:/Lab_Member/Tobi/Experiments/Exp9_Social-Stress/Raw Data/Behavior/", batch, "/SocP/SLEAP/output/", socpPhase)
+    novelLocDir <- paste0("S:/Lab_Member/Tobi/Experiments/Exp9_Social-Stress/Raw Data/Behavior/", batch, "/SocP")
+    
+    # Create output directory if it doesn't exist
+    if (!dir.exists(outputDir)) {
+      dir.create(outputDir)
+    }
+    
+    # Read in animal ID and Code list from .txt file
+    animalIDCode <- read.table("S:/Lab_Member/Tobi/Experiments/Exp9_Social-Stress/Planning/animalIDCode.txt", header = TRUE)
+    
+    # Create a new folder for saving the plots
+    plotDir <- file.path(outputDir, "plots")
+    dir.create(plotDir, showWarnings = FALSE)
+    
+    # Get a list of CSV files in the input directory
+    fileList <- list.files(path = inputDir, pattern = "*.csv")
+    
+    # Read novelLoc file
+    novelLoc <- read.table(file.path(novelLocDir, paste0("novelLoc", socpPhase, ".txt")), header = TRUE, sep = "\t")
+    
+    # Create new list dfList
+    dfList <- list()
+    
+    # Loop through each file in the input directory
+    for (file in fileList) {
+      # Read in tracking data and get names of variables in the data frame
+      inputFile <- file.path(inputDir, file)
+      tracking <- ReadDLCDataFromCSV(file = inputFile, fps = 30)
+      
+      # Extract the input file name without the extension
+      inputFileName <- sub(".csv$", "", basename(inputFile))
+      
+      # Calibrate the tracking data, add zones, and plot the zones
+      tracking <- CalibrateTrackingData(tracking, method = "area", in.metric = 44 * 24, points = c("tl", "tr", "br", "bl"))
+      tracking <- AddOFTZones(tracking, scale_center = 0.5, scale_periphery = 0.8, scale_corners = 0.4, points = c("tl", "tr", "br", "bl"))
+      PlotZones(tracking)
+      PlotPointData(tracking, points = c("nose"))
+      
+      tracking <- OFTAnalysis(tracking, points = "bodycentre", movement_cutoff = 5, integration_period = 5)
+      
+      # Extract code from filename
+      code <- str_extract(inputFileName, "^[A-Za-z0-9]{4}")
+      
+      # Calculate time in contact with novel and familiar objects
+      if (code %in% novelLoc$Code) {
+        totalTime <- 10 * 60
+        
+        if (novelLoc[novelLoc$Code == code, "NovelLoc"] == "R") {
+          isInZoneLeft <- GetDistances(tracking, "socl", "nose") <= 6 & GetDistances(tracking, "socl", "bodycentre") > 1
+          isInZoneRight <- GetDistances(tracking, "socr", "nose") <= 6 & GetDistances(tracking, "socr", "bodycentre") > 1
+          contactNov <- sum(isInZoneLeft) * totalTime / length(isInZoneLeft)
+          contactFam <- sum(isInZoneRight) * totalTime / length(isInZoneRight)
+        } else {
+          isInZoneLeft <- GetDistances(tracking, "socl", "nose") <= 6 & GetDistances(tracking, "socl", "bodycentre") > 1
+          isInZoneRight <- GetDistances(tracking, "socr", "nose") <= 6 & GetDistances(tracking, "socr", "bodycentre") > 1
+          contactFam <- sum(isInZoneLeft) * totalTime / length(isInZoneLeft)
+          contactNov <- sum(isInZoneRight) * totalTime / length(isInZoneRight)
+        }
+        
+        contactLeft <- sum(isInZoneLeft) * totalTime / length(isInZoneLeft)
+        contactRight <- sum(isInZoneRight) * totalTime / length(isInZoneRight)
+      } else {
+        contactLeft <- 0
+        contactRight <- 0
+      }
+      
+      # Check if the nose is in the contact zone of objR
+      isContactsocr <- ifelse(isInZoneRight, "contact socr", "")
+      # Check if the nose is in the contact zone of objL
+      isContactsocl <- ifelse(isInZoneLeft, "contact socl", "")
+      
+      # Calculate time with nose between 6-10 cm of either objL or objR
+      if (code %in% novelLoc$Code) {
+        totalTime <- 10 * 60
+        if (novelLoc[novelLoc$Code == code, "NovelLoc"] == "R") {
+          isInProxLeft <- GetDistances(tracking, "socl", "nose") > 6 & GetDistances(tracking, "socl", "nose") <= 10
+          isInProxRight <- GetDistances(tracking, "socr", "nose") > 6 & GetDistances(tracking, "socr", "nose") <= 10
+          proxNov <- sum(isInProxLeft) * totalTime / length(isInProxLeft)
+          proxFam <- sum(isInProxRight) * totalTime / length(isInProxRight)
+        } else {
+          isInProxLeft <- GetDistances(tracking, "socl", "nose") > 6 & GetDistances(tracking, "socl", "nose") <= 10
+          isInProxRight <- GetDistances(tracking, "socr", "nose") > 6 & GetDistances(tracking, "socr", "nose") <= 10
+          proxFam <- sum(isInProxLeft) * totalTime / length(isInProxLeft)
+          proxNov <- sum(isInProxRight) * totalTime / length(isInProxRight)
+        }
+      } else {
+        proxNov <- 0
+        proxFam <- 0
+      }
+      
+      # Calculate frequency of zone visits for objL and nose
+      isInZoneVisitL <- cumsum(GetDistances(tracking, "socl", "nose") <= 6) > 0 & c(0, diff(GetDistances(tracking, "socl", "nose") <= 6)) == 1
+      frequencyL <- sum(isInZoneVisitL)
+      isInZoneVisitR <- cumsum(GetDistances(tracking, "socr", "nose") <= 6) > 0 & c(0, diff(GetDistances(tracking, "socr", "nose") <= 6)) == 1
+      frequencyR <- sum(isInZoneVisitR)
+      
+      # Create a data frame with the timeLeft, timeRight, timeNoseLeft, and timeNoseRight values
+      df <- data.frame(
+        file = inputFileName,
+        ID = animalIDCode[animalIDCode$Code == code, "ID"],
+        Code = code,
+        contactNovel = contactNov,
+        contactFamiliar = contactFam,
+        proxNovel = proxNov,
+        proxFamiliar = proxFam,
+        contactLeft = contactLeft,
+        contactRight = contactRight,
+        frequencyLeft = frequencyL,
+        frequencyRight = frequencyR,
+        distance = tracking$Report$bodycentre.raw.distance,
+        stationary = tracking$Report$bodycentre.time.stationary,
+        speedMoving = tracking$Report$bodycentre.speed.moving,
+        speedRaw = tracking$Report$bodycentre.raw.speed
+      )
+      
+      # Construct the output file name
+      outputFile <- paste0(inputFileName, "_output.csv")
+      
+      # Write the data frame to a csv file with the constructed file name
+      write.csv(df, outputFile, row.names = FALSE)
+      
+      # Add the current data frame to the list
+      dfList[[length(dfList) + 1]] <- df
+      
+      # Plot the density paths for bodycentre
+      plots <- PlotDensityPaths(tracking, points = c("bodycentre"))
+      
+      # Set the file name for the plot image
+      plotFile <- file.path(plotDir, paste0(inputFileName, "_DensityPath.png"))
+      
+      # Save the plot image with the file name
+      ggsave(plotFile, plot = plots$bodycentre, width = 7, height = 4)
+      
+      # Print progress message
+      cat(sprintf("Processed file %s\n", file))
+    }
+    
+    # Combine all data frames into a single data frame
+    dfCombined <- do.call(rbind, dfList)
+    
+    # Write the combined data frame to a csv file
+    write.csv(dfCombined, file.path(outputDir, "combined_output.csv"), row.names = FALSE)
+    
+    # Print "done" message
+    cat("Processing complete for batch", batch, "and socpPhase", socpPhase, "\n")
+  }
 }
-
-# Combine all data frames into a single data frame
-df_combined <- do.call(rbind, df_list)
-
-# Write the combined data frame to a csv file
-write.csv(df_combined, file.path(output_dir, "combined_output.csv"), row.names = FALSE)
-
-# Print "done" message
-cat("done\n")
